7e0c832174026d212fbbd70c90abedae0b4af2d5
unknown <99067235@mydavinci.nl>
2021-10-27 13:20:16 +0200

    update

--- Papi Gelato.py
+++ Papi Gelato.py
@@ -1,32 +1,20 @@
 # Jurrian Schouten
 # 99067235
-kiezen = False
+
+e = 0
 
 def bakjeKiezen():
     antwoord = input("Wilt u deze {antwoord} bolletje(s) in A) een hoorntje of B) een bakje? ")
-    
-    if antwoord == "A" or "B":
-        kiezen = True
-    else:
-        kiezen = False
-
-while kiezen == False:
-    antwoord = input("Wilt u deze {antwoord} bolletje(s) in A) een hoorntje of B) een bakje? ")
-    
-    if antwoord == "A" or "B":
-        kiezen = True
-    else:
-        kiezen = False
-    
-
 
 def sorry():
     print("Sorry, zulke grote bakken hebben we niet")
-
+    
 print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
-antwoord = int(input("Hoeveel bolletjes wilt u? "))
-if antwoord <= 3 and antwoord >= 0:
-    bakjeKiezen()
-else:
-    sorry()
+while e == 0:
+    antwoord = int(input("Hoeveel bolletjes wilt u? "))
+    if antwoord <= 3 and antwoord >= 0:
+        bakjeKiezen()
+        break
+    else:
+        sorry()
