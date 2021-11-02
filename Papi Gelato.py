39825eb13b48e8e4c5142ffd9cf80a27c16ee199
unknown <99067235@mydavinci.nl>
2021-11-02 11:24:22 +0100

    smaken toegevoegd

--- Papi Gelato.py
+++ Papi Gelato.py
@@ -12,18 +12,27 @@ print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille 
 while e == 0:
     antwoord = int(input("Hoeveel bolletjes wilt u? "))
     if antwoord <= 3 and antwoord >= 0:
-        antwoord2 = input("Wilt u deze "+ str(antwoord) +" bolletje(s) in A) een hoorntje of B) een bakje? ").upper()
-        if antwoord2 == "A" or "B":
-            antwoord3 = input("Hier is uw hoorntje/bakje met "+ str(antwoord) +" bolletje(s). Wilt u nog meer bestellen? (Y/N)").upper()
-            if antwoord3 == "Y":
-                print("")
-            elif antwoord3 == "N":
-                print("Bedankt en tot ziens!")
-                break
+        for i in range(1,antwoord + 1):
+            print("Welke smaak wilt u voor bolletje nummer", i, "A) Aardbei, C) Chocolade, M) Munt of V) Vanille?")
+            antwoord2 = input("Vul hier uw antwoord in:" ).upper()
+        if antwoord2 == "A" or "C" or "M" or "V":
+            antwoord3 = input("Wilt u deze "+ str(antwoord) +" bolletje(s) in A) een hoorntje of B) een bakje? ").upper()
+            if antwoord3 == "A" or "B":
+                antwoord4 = input("Hier is uw hoorntje/bakje met "+ str(antwoord) +" bolletje(s). Wilt u nog meer bestellen? (Y/N)").upper()
+                if antwoord4 == "Y":
+                    print("")
+                elif antwoord4 == "N":
+                    print("Bedankt en tot ziens!")
+                    break
+                else:
+                    print("Sorry, dat snap ik niet...")
+                    break
             else:
-                print("Sorry, dat snap ik niet...")
-                break
+                sorry() 
         else:
-            sorry() 
+            sorry()
     else:
         sorry()
+
+    
+
