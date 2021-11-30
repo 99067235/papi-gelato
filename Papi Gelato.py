ff45ca8ad193c15b89b9dd5fd68193fc32c739eb
unknown <99067235@mydavinci.nl>
2021-11-30 11:23:18 +0100

    bugs gefixt

--- Papi Gelato.py
+++ Papi Gelato.py
@@ -22,6 +22,7 @@ kostenToppings = 0
 kostenBolletjes = 0
 HoeveelLiter = 0
 smaak = ""
+antwoord3 = ""
 
 def sorry():
     print("Sorry, zulke grote bakken hebben we niet")
@@ -35,6 +36,7 @@ def snapNiet():
 def topping(bolletjes):
         global aantalToppings
         global toppingKosten
+        global antwoord3
         toppingKeuze = input("Wat voor topping wilt u: G) Geen, SR) Slagroom, SP) Sprinkels of CS) Caramel Saus? ").lower()
         if toppingKeuze == "g":
             print("")
@@ -49,10 +51,10 @@ def topping(bolletjes):
                 toppingKosten += (0.30 * bolletjes)
 
             elif toppingKeuze == "cs" or toppingKeuze == "caramelsaus":
-                if toppingKeuze == "B":
+                if antwoord3 == "B":
                     toppingKosten += 0.90
 
-                elif toppingKeuze == "A":
+                elif antwoord3 == "A":
                     toppingKosten += 0.60
                 
                 else:
@@ -62,6 +64,7 @@ def topping(bolletjes):
                 snapNiet()
         else:
             snapNiet()
+            topping(bolletjes, antwoord3)
 
 def bonParticulier():
     global kostenBakje
@@ -104,7 +107,7 @@ def bonZakelijk():
 def bakjeGekozen(bolletjes):
     global bakje
     bakje += 1
-    topping()
+    topping(bolletjes)
     antwoord4 = input("Hier is uw bakje met "+ str(bolletjes) +" bolletje(s). Wilt u nog meer bestellen? (Y/N)").upper()
     if antwoord4 == "Y":
         print("")
@@ -112,24 +115,31 @@ def bakjeGekozen(bolletjes):
         bonParticulier()
     else:
         sorry()
+        bakjeGekozen(bolletjes)
+
+def hoorntjeMeerbestellen(bolletjes):
+    antwoord4 = input("Hier is uw hoorntje met "+ str(bolletjes) +" bolletje(s). Wilt u nog meer bestellen? (Y/N)").upper()
+    if antwoord4 == "Y":
+        print("")
+    elif antwoord4 == "N":
+        bonParticulier()
+    else:
+        snapNiet()
+        hoorntjeMeerbestellen()
 
 def keuzeVerpakking(bolletjes):
     global hoorntje
+    global antwoord3
     antwoord3 = input("Wilt u deze "+ str(bolletjes) +" bolletje(s) in A) een hoorntje of B) een bakje? ").upper()
     if antwoord3 == "A":
         hoorntje += 1
         topping(bolletjes)
-        antwoord4 = input("Hier is uw hoorntje met "+ str(bolletjes) +" bolletje(s). Wilt u nog meer bestellen? (Y/N)").upper()
-        if antwoord4 == "Y":
-            print("")
-        elif antwoord4 == "N":
-            bonParticulier()
-        else:
-            snapNiet()
+        hoorntjeMeerbestellen(bolletjes)
     elif antwoord3 == "B":
-        bakjeGekozen()
+        bakjeGekozen(bolletjes)
     else:
         snapNiet()
+        keuzeVerpakking(bolletjes)
 
 def welkeSmaak(bolletjes):
     for i in range(1,bolletjes + 1):
@@ -139,8 +149,10 @@ def welkeSmaak(bolletjes):
         keuzeVerpakking(bolletjes)
     else:
         snapNiet()
+        welkeSmaak(bolletjes)
 
 def bakjeMeerBestellen(bolletjes):
+    global antwoord3
     topping(bolletjes)
     antwoord4 = input("Hier is uw bakje met "+ str(bolletjes) +" bolletje(s). Wilt u nog meer bestellen? (Y/N)").upper()
     if antwoord4 == "Y":
@@ -150,10 +162,12 @@ def bakjeMeerBestellen(bolletjes):
         e = 1
     else:
         snapNiet()
+        bakjeMeerBestellen(bolletjes)
 
 def welkeSmaakGroot(bolletjes):
     global bakje
     global smaak
+    global antwoord3
     bakje += 1
     for i in range(1,bolletjes + 1):
         print("Welke smaak wilt u voor bolletje nummer", str(i), "A) Aardbei, C) Chocolade, of V) Vanille?")
@@ -162,57 +176,66 @@ def welkeSmaakGroot(bolletjes):
         bakjeMeerBestellen(bolletjes)
     else:
         snapNiet()
+        welkeSmaakGroot(bolletjes, antwoord3)
 
 
 def aantalbolletjes():
     global totaalBolletjes
-    bolletjes = int(input("Hoeveel bolletjes wilt u? "))
-    if bolletjes <= 3 and bolletjes >= 0:
-        totaalBolletjes += bolletjes
-        welkeSmaak(bolletjes)
-    elif bolletjes >= 3 and bolletjes <= 8:
-        totaalBolletjes += bolletjes
-        print("Dan krijgt u van mij een bakje met", bolletjes, "bolletjes")
-        welkeSmaakGroot(bolletjes)
-    elif bolletjes <= 0:
-        print("Helaas verkopen wij geen hoorntjes of bakjes met minder dan 1 bolletje.")
-        bolletjes = 0
-    elif bolletjes > 8:
-        sorry()
-        bolletjes = 0
-    else:
+    global antwoord3
+    try:
+        bolletjes = int(input("Hoeveel bolletjes wilt u? "))
+        if bolletjes <= 3 and bolletjes >= 0:
+            totaalBolletjes += bolletjes
+            welkeSmaak(bolletjes)
+        elif bolletjes >= 3 and bolletjes <= 8:
+            totaalBolletjes += bolletjes
+            print("Dan krijgt u van mij een bakje met", bolletjes, "bolletjes")
+            welkeSmaakGroot(bolletjes)
+        elif bolletjes <= 0:
+            print("Helaas verkopen wij geen hoorntjes of bakjes met minder dan 1 bolletje.")
+            bolletjes = 0
+        elif bolletjes > 8:
+            sorry()
+            aantalbolletjes()
+            bolletjes = 0
+        else:
+            snapNiet()
+            bolletjes = 0
+    except ValueError:
         snapNiet()
-        bolletjes = 0
+        aantalbolletjes()
 
 def hoeveelLiter():
     global HoeveelLiter
-    HoeveelLiter = int(input("Hoeveel liter ijs wilt u bestellen? "))
-    if HoeveelLiter >= 1:
-        for f in range(1, HoeveelLiter + 1):
-            print("Welke smaak wilt u voor liter", (f), "A) Aardbei, C) Chocolade, of V) Vanille?")
-            welkeSmaak = input("Vul hier uw antwoord in: ").upper()
-        if welkeSmaak == "A" or welkeSmaak == "C" or welkeSmaak == "M" or welkeSmaak == "V":
-            bonZakelijk()
+    try:
+        HoeveelLiter = int(input("Hoeveel liter ijs wilt u bestellen? "))
+        if HoeveelLiter >= 1:
+            for f in range(1, HoeveelLiter + 1):
+                print("Welke smaak wilt u voor liter", (f), "A) Aardbei, C) Chocolade, of V) Vanille?")
+                welkeSmaak = input("Vul hier uw antwoord in: ").upper()
+            if welkeSmaak == "A" or welkeSmaak == "C" or welkeSmaak == "M" or welkeSmaak == "V":
+                bonZakelijk()
+            else:
+                snapNiet()
+        elif HoeveelLiter <= 0:
+            print("Helaas verkopen wij geen hoorntjes of bakjes met minder dan 1 bolletje.")
         else:
             snapNiet()
-    elif HoeveelLiter <= 0:
-        print("Helaas verkopen wij geen hoorntjes of bakjes met minder dan 1 bolletje.")
-    else:
+    except ValueError:
         snapNiet()
+        hoeveelLiter()
 
 def welkom():
     global i
-    global bolletjes
+    global antwoord3
     print("Welkom bij Papi Gelato")
     while i == 0:
-        particulierOfZakelijk = int(input("Bent u 1) particulier of 2) zakelijk? "))
-        if particulierOfZakelijk == 1:
+        particulierOfZakelijk = input("Bent u p) particulier of z) zakelijk? ").lower()
+        if particulierOfZakelijk == "p" or particulierOfZakelijk == "particulier":
             aantalbolletjes()
-        elif particulierOfZakelijk == 2:
+        elif particulierOfZakelijk == "z" or particulierOfZakelijk == "zakelijk":
             hoeveelLiter()
         else:
             snapNiet()
-
-
 
 welkom()
